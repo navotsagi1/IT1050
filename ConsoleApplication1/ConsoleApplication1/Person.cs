5bd810d2cdc273245a343f383f282e557c00ba20
Navot Saggi <navotsagi1@gmail.com>
2016-02-18 11:09:36 -0800

    First Assignment
    IT-1050, first assignment.

---
+++ ConsoleApplication1/ConsoleApplication1/Person.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Person
+    {
+        public string Firstname;
+        public string Middleinitial;
+        public string Lastname;
+        public int Age;
+        public int HeightFeet;
+        public int HeightInches;
+        public bool IsCitizen;
+
+        public string NoInitial() 
+        {
+        if (Middleinitial == "none") 
+        {
+                return Middleinitial = null;
+        }
+            else
+             {
+                return Middleinitial;
+             }
+        }
+        
+        public string fullname() 
+        {
+            return Firstname + " " + NoInitial() + " " + Lastname;
+        }
+
+        public bool QIsCitizen(string x)
+        {
+            if(x == "yes") 
+            {
+                return IsCitizen = true;
+            }
+            else 
+            {
+                return IsCitizen = false;
+            }
+        }
+
+        public double InchtoCM()
+        {
+            return 30.48 * HeightFeet + 2.54 * HeightInches;
+        }
+
+        public string CanVote()
+        {
+            if ((IsCitizen == true) && (Age >= 18))
+            {
+               return (fullname() + " can vote");
+            }
+            else
+            {
+                return (fullname() + " cannot vote");
+            }
+        }
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            System.Console.WriteLine("Enter first name here");
+            person.Firstname = System.Console.ReadLine();
+            System.Console.WriteLine("Enter middle initial here,(if none, enter none)");
+            person.Middleinitial = System.Console.ReadLine();
+            System.Console.WriteLine("Enter last name here");
+            person.Lastname = System.Console.ReadLine();
+
+            System.Console.WriteLine("Enter Age here");
+            person.Age = int.Parse(System.Console.ReadLine());
+            System.Console.WriteLine("Are you a U.S. citizen?,(yes or no).");
+            person.QIsCitizen(System.Console.ReadLine());
+
+
+            System.Console.WriteLine("Enter Height, (feet), in here.");
+            person.HeightFeet = int.Parse(System.Console.ReadLine());
+            System.Console.WriteLine("Enter height, (inches), in here.");
+            person.HeightInches = int.Parse(System.Console.ReadLine());
+
+
+            System.Console.WriteLine("Name: " + person.fullname());
+            System.Console.WriteLine("Height(cm): " + person.InchtoCM());
+            System.Console.WriteLine("Voting Status: " + person.CanVote());
+
+
+            System.Console.WriteLine("Press any key to continue......");
+            Console.ReadKey();
+        }
+    }
+}
