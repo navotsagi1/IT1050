6e534d957731b8056933a54e8eede716f122fb2d
Navot Saggi <navotsagi1@gmail.com>
2016-05-03 11:19:11 -0700

    project 4

---
+++ Assignment 4/Assignment 4/Concessions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_4
+{
+    class Concessions
+    {
+        private string Name;
+        private double Price;
+        public double GetPrice() { return Price; }
+        public string GetName() { return Name; }
+
+        public Concessions (string name, double price)
+        {
+            Price = price;
+            Name = name;
+        }
+        
+    }
+}
