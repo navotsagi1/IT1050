041a9af295a8a4f88b80146c390ce71ab577f215
Navot Saggi <navotsagi1@gmail.com>
2016-05-04 18:16:37 -0700

    Assignment 4

---
+++ Assignment 4/Assignment 4/Customer.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_4
+{
+    class Customer
+    {
+        private string Name;
+        private string AgeStatus;
+        private double Money;
+        List<string> WhatIBought = new List<string>();
+
+        public Customer(string name, string agestatus, double money)
+        {
+            Name = name;
+            AgeStatus = agestatus;
+            Money = money;
+        }
+
+        public void FreeStuff(string Item, double DiscountPrice)
+        {
+            string A = System.Console.ReadLine();
+            if ((A == "Yes") || (A == "yes"))
+            {
+                Money -= DiscountPrice;
+                WhatIBought.Add(Item);
+            }
+        }
+
+
+        public void BuyFood(Concessions Item, int Amount)
+        {
+
+            Money -= Item.GetPrice() * Amount;
+            WhatIBought.Add(Item.GetName());
+
+            if ((Item.GetName() == "Candy") && (Amount == 3))
+            {
+                System.Console.WriteLine("Would You like an aditional free candy?");
+                FreeStuff("Candy", 0);
+            }
+
+        }
+
+
+        public void BuyTicket(Ticket ticket, int Amount)
+        {
+            Money -= ticket.GetPrice() * Amount;
+            WhatIBought.Add(ticket.GetAge() + " " + ticket.GetTime() + " ticket.");
+
+            if ((Amount == 3) && (ticket.GetTime() == "Evening"))
+            {
+                System.Console.WriteLine("Would you like a free bug of popcorn?");
+                FreeStuff("Popcorn", 0);
+            }
+
+            if ((WhatIBought.Contains("Popcorn")) && (WhatIBought.Contains("Large Soda")))
+            {
+                System.Console.WriteLine("Would You like an aditional movie ticket with 2$ discount?");
+                FreeStuff(ticket.GetName(), (ticket.GetPrice() - 2));
+            }
+
+        }
+    }
+}
---
+++ Assignment 4/Assignment 4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Ticket ChildMaitnee = new Ticket("Child", 3.99, "Maitnee");
+            Ticket AdultMaitnee = new Ticket("Adult", 5.99, "Maitnee");
+            Ticket SeniorMaitnee = new Ticket("Senior", 4.50, "Maitnee");
+
+            Ticket ChildEvning = new Ticket("Child", 6.99, "Evning");
+            Ticket AdultEvning = new Ticket("Adult", 10.99, "Evning");
+            Ticket SeniorEvning = new Ticket("Senior", 8.50, "Evning");
+
+            //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+            Concessions SmallSoda = new Concessions("SmallSoda", 3.50);
+            Concessions LargeSoda = new Concessions("Large Soda", 5.99);
+            Concessions HotDog = new Concessions("Hot Dog", 3.99);
+            Concessions Popcorn = new Concessions("Popcorn", 4.50);
+            Concessions Candy = new Concessions("Candy", 1.99);
+
+
+            System.Console.ReadKey();
+            
+        }
+    }
+}
